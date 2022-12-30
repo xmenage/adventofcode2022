8a41e85d33de5488794043c606513f102a6824f3
Xavier Ménage <xmenage@yahoo.com>
2022-12-30 13:19:10 +0100

    day 12 part 2

--- 12/adventofcode12part2.ex
+++ 12/adventofcode12part2.ex
@@ -1,33 +1,13 @@
+# for part 2, we just need to reverse the algorithm : start from the 'E' point and search downward until reaching at least one point
+# at elevation 'a'.
+# rules of propagation are also reverse :
+# move at most 1 level downward
+# move any value upward.
+
 defmodule Main do
   def parseInput(inputFileName) do
     File.stream!(inputFileName)
     |> Stream.map(&String.trim(&1)) |> Enum.map(&(String.to_charlist(&1)))
-  end
-
-  def makeEmptyResultMatrix(width, height) do
-    emptyRow = for _n <- 1..width, do:  {-1, false, false}
-    for _n <- 1..height, do: emptyRow
-  end
-
-  # propagate expansion as long as there is no obstacle on the row. This is the heart of the algorithm
-  def expandRow(steps, altitude, expandingDirection , inputRow, outputRow) do
-    Enum.zip_reduce([inputRow,outputRow],{steps, altitude, false, []}, fn [inputaltitude, {currentsteps, alreadyprocessed, expandDirection}], acc ->
-      {previousSteps, previousAltitude, stopExpansion, outputRow} = acc
-      newoutputRow = cond do
-        # if propagation has already been blocked, then just return current point value, first two elements set to 0 as we won't need this on remainder of enum
-        stopExpansion -> {0,0, stopExpansion, [{currentsteps, alreadyprocessed, expandDirection} | outputRow]}
-        # if current point has already been expanded, then stop propagation - actually not sure we need this
-        # alreadyprocessed -> {0,0, true, [{currentsteps, alreadyprocessed, expandDirection} | outputRow]}
-        # if difference of altitude is more than 1, then stop propagation and don't change points on remainder of the row
-        abs(inputaltitude-previousAltitude) > 1 -> {0,0,true, [{currentsteps, alreadyprocessed, expandDirection} | outputRow]}
-        # if current point has not been touched yet, then propagate steps from previous point, and set processed status to false to have it expanded later.
-        # set its future expansion direction perpendicular to current
-        currentsteps == -1 -> {previousSteps+1,inputaltitude,false, [{previousSteps+1, false, not expandingDirection} | outputRow]}
-        # if current point has already been visited, but number of steps is higher than this new number of steps, then replace and reset expansion status and direction
-        previousSteps+1 < currentsteps -> {previousSteps+1,inputaltitude,false, [{previousSteps+1, false, not expandingDirection} | outputRow]}
-      end
-      Enum.reverse(newoutputRow)
-    end)
   end
 
   def createResultMatrix(inputMatrix) do
@@ -35,21 +15,27 @@ defmodule Main do
       row |> Enum.map(fn elevation ->
         cond do
           elevation >= ?a && elevation <= ?z -> {elevation, -1}
-          elevation == ?S -> {?a, 0}
-          elevation == ?E -> {?z, -1}
+          elevation == ?S -> {?a, -1}
+          elevation == ?E -> {?z, 0}
         end
       end)
     end)
   end
 
+  # This is the main part of the algorithm : scan a row both ways and propagate number of steps when possible,
+  # increment number of steps at each step.
   def propagateForthAndBack(row) do
     for _n <- 1..2, reduce: row do
         rowAcc ->
           {_, rowOut} = rowAcc |> Enum.reduce({{-1, 0}, []}, fn {currentelevation, currentsteps}, {{previouselevation, previoussteps}, outputRow} ->
             cond do
-              previouselevation == -1 -> {{currentelevation, currentsteps}, [{currentelevation, currentsteps}]} #first element in the row
-              currentelevation > previouselevation + 1 -> {{currentelevation, currentsteps}, [{currentelevation, currentsteps} | outputRow]} #current point elevation is more than 1 above previous point, don't change current point
-              previoussteps >= 0 && (currentsteps == -1 || previoussteps+1 < currentsteps) -> {{currentelevation, previoussteps+1}, [{currentelevation, previoussteps+1} | outputRow]} #if previous point has been reached (steps>0) and current point has not been reached or number of steps is higher, then propagate to current point
+              #first element in the row (accumulator is initiated with negative -1 elevation)
+              previouselevation == -1 -> {{currentelevation, currentsteps}, [{currentelevation, currentsteps}]}
+              #if current point elevation is more than 1 above previous point, don't change current point
+              currentelevation < previouselevation - 1 -> {{currentelevation, currentsteps}, [{currentelevation, currentsteps} | outputRow]}
+              #if previous point has been reached (steps>0) and current point has not been reached or number of steps is higher, then propagate to current point
+              previoussteps >= 0 && (currentsteps == -1 || previoussteps+1 < currentsteps) -> {{currentelevation, previoussteps+1}, [{currentelevation, previoussteps+1} | outputRow]}
+              #otherwise don't change current point
               true -> {{currentelevation, currentsteps}, [{currentelevation, currentsteps} | outputRow]} #else do nothing, go to next
             end
           end)
@@ -57,13 +43,8 @@ defmodule Main do
     end
   end
 
-  # reuse from day 8
   def transpose(matrix) do
     Enum.zip_with(matrix, &(&1))
-  end
-
-  def flipHorizontal(matrix) do
-    matrix |> Enum.map(&(Enum.reverse(&1)))
   end
 
 end
@@ -98,7 +79,13 @@ stepsMatrix = result |> Enum.map(fn row ->
 end)
 # IO.inspect(stepsMatrix, [label: "stepsMatrix", limit: :infinity, charlists: :as_charlists])
 
-# find End point from input matrix and extract number of steps from output matrix
+# find all points at elevation a that have been reached and sort the list to find the minimum number of steps
 indexEndPoint = inputMatrix |> List.flatten |> Enum.find_index(&(&1 == ?E))
-{_, stepsToEndPoint} = result |> List.flatten() |> Enum.at(indexEndPoint)
-IO.puts("index end point : #{indexEndPoint}, stepsToEndPoint= #{stepsToEndPoint}")
+{_, shortestpathfromminimumelevation} = result
+  |> List.flatten()
+  |> Enum.filter(fn {elevation, steps} -> elevation == ?a && steps >= 0 end)
+  |> Enum.sort(fn {_, step1}, {_, step2} -> step1 < step2 end )
+  |> IO.inspect(label: "points at lowest elevation")
+  |> List.first(0)
+
+  IO.puts("shortest path from 'a' elevation to the top #{shortestpathfromminimumelevation}")
